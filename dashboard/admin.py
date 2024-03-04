bd1fc7e4d619667de69d57fe01f4ccd9e95f5449
byron00123 <omondibyron25@gmail.com>
2024-03-04 12:54:31 +0300

    add filter by category

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
 
+
+admin.site.site_header = 'ByronInventory Dashboard'
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category','quantity')
+    list_filter = ['category']
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+#admin.site.unregister(Group)
