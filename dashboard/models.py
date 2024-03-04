65bba8cb0e1dea6235ae9de2732d194c7872e1d4
byron00123 <omondibyron25@gmail.com>
2024-03-04 13:14:54 +0300

    create order model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY = (
@@ -15,6 +16,16 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}-{self.quantity}'
+    
+
+class Order(models.Model): 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null = True)
+    staff = models.ForeignKey(User, models.CASCADE, null = True) 
+    order_quantity = models.PositiveBigIntegerField (null = True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
         
 
 
