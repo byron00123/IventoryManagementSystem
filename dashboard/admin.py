03c5def566f08dea74bca9d940597b7e8e14477a
byron00123 <omondibyron25@gmail.com>
2024-03-04 13:17:52 +0300

    register order model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 
@@ -13,4 +13,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['category']
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
 #admin.site.unregister(Group)
