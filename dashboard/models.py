2934fac7c2040b0a824b2da9020747dd6592869a
byron00123 <omondibyron25@gmail.com>
2024-03-04 10:58:48 +0300

    create model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length =100, null=True)
+    category = models.CharField(max_length=20, choices = CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
