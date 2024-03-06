5f6c53c0e0b13be3e9c82a2a76b563aa5b035fe0
byron00123 <omondibyron25@gmail.com>
2024-03-06 09:49:43 +0300

    create user app

--- dashboard/models.py
+++ dashboard/models.py
@@ -14,6 +14,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(null=True)
 
 
+    class Meta:
+        verbose_name_plural = 'Product'
+
+
     def __str__(self):
         return f'{self.name}-{self.quantity}'
     
@@ -23,6 +27,9 @@ class Order(models.Model):
     staff = models.ForeignKey(User, models.CASCADE, null = True) 
     order_quantity = models.PositiveBigIntegerField (null = True)
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Order'
 
     def __str__(self):
         return f'{self.product} ordered by {self.staff.username}'
