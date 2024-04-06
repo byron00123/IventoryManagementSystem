3c9ce2b9f86b0ab7a8be9af982520027bb54e445
byron00123 <omondibyron25@gmail.com>
2024-04-06 09:47:03 +0300

    add profile page

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
+    path('profile/', user_view.register, name='user-profile'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
+    
 ]
