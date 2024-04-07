9889f7c7e43cb74daccf193b6df0615869cbe69a
byron00123 <omondibyron25@gmail.com>
2024-04-07 06:16:55 +0300

    create profile view

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
-    path('profile/', user_view.register, name='user-profile'),
+    path('profile/', user_view.profile, name='user-profile'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
     
