23edc96011599716b617c7ffd28ded13cda6a446
byron00123 <omondibyron25@gmail.com>
2024-04-08 12:05:03 +0300

    update user profile

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('dashboard', views.index, name= 'dashboard-index'),
     path('staff/', views.staff, name = 'dashboard-staff'),
-     path('product/', views.product, name = 'dashboard-product'),
-      path('order/', views.order, name = 'dashboard-order'),
+    path('product/', views.product, name = 'dashboard-product'),
+    path('order/', views.order, name = 'dashboard-order'),
 ]
--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -18,3 +19,14 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username', 'email']
 
 
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone', 'image']        
