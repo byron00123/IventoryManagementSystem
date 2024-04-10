5d859b0eec8ff4fd790120bb73229e97e867ada1
byron00123 <omondibyron25@gmail.com>
2024-04-10 11:03:08 +0300

    pushing to render

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k6zux#%ex^-e5efijo68r52p-w24eo3y^s5r)fq9d8=^tq^ce)'
+#SECRET_KEY = 'django-insecure-k6zux#%ex^-e5efijo68r52p-w24eo3y^s5r)fq9d8=^tq^ce)'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-k6zux#%ex^-e5efijo68r52p-w24eo3y^s5r)fq9d8=^tq^ce')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
