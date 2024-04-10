dc4c1ad1a5b7d0936979753154013e125e729973
byron00123 <omondibyron25@gmail.com>
2024-04-10 10:47:19 +0300

    pushing to render

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url # type: ignore
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k6zux#%ex^-e5efijo68r52p-w24eo3y^s5r)fq9d8=^tq^ce)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','inventorymanagementsystem-dzgo.onrender.com']
 
@@ -78,13 +80,21 @@ WSGI_APPLICATION = 'inventoryproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+if not DEBUG:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
-DATABASES = {
+
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}   
+
+
 
 
 # Password validation
